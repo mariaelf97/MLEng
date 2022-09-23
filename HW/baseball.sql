35338e313289311a6b97bd5e63b47c02dc5b6d1d
Maryam Ahmadi J <39787943+mariaelf97@users.noreply.github.com>
2022-09-23 11:21:58 -0700

    fixed the query , added sum function

--- HW/baseball.sql
+++ HW/baseball.sql
@@ -8,13 +8,13 @@ alter table joined_batter_game modify batter varchar(100);
 -- calculate annual batting average
 drop temporary table if exists annual_at_bat_per_player;
 create temporary table annual_at_bat_per_player
-select batter,year(local_date), Hit/nullif(atBat,0) as Average_at_bat from joined_batter_game group by batter,year(local_date) order by year(local_date);
+select batter,year(local_date), sum(Hit)/sum(nullif(atBat,0)) as Average_at_bat from joined_batter_game group by batter,year(local_date) order by year(local_date);
 -- view the table annual_at_bat_per_player
 select * from annual_at_bat_per_player order by batter limit 1,20;
 -- calculate historic batting average
 drop temporary table if exists historic_at_bat_per_player;
 create temporary table historic_at_bat_per_player
-select batter, Hit/nullif(atBat,0) as Average_at_bat from joined_batter_game group by batter;
+select batter, sum(Hit)/sum(nullif(atBat,0)) as Average_at_bat from joined_batter_game group by batter;
 -- view the table historic_at_bat_per_player
 select * from historic_at_bat_per_player order by batter limit 1,20;
 -- calculate last 100 days batting average, first we create a table with the 100 recent entries for each player
@@ -24,6 +24,6 @@ select * from (select * , row_number() over (partition by batter order by local_
 -- Then we calculate the batting average for each player
 drop temporary table if exists last_100_days_batting_per_player;
 create temporary table last_100_days_batting_per_player
-select batter, Hit/nullif(atBat,0) as batting_average from last_100_days_per_player group by batter;
+select batter, sum(Hit)/sum(nullif(atBat,0)) as batting_average from last_100_days_per_player group by batter;
 --view the table last_100_days_batting_per_player
 select * from last_100_days_batting_per_player order by batter limit 1,20;

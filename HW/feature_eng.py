be5ccc60194f103b6c16057922647ea78e7046ca
Maryam Ahmadi J <39787943+mariaelf97@users.noreply.github.com>
2022-10-13 14:15:13 -0700

    regression analysis added

--- HW/feature_eng.py
+++ HW/feature_eng.py
@@ -3,13 +3,14 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import statsmodels.api as sm
 
 # user input to receive dataset
 # https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
 dataset = pd.read_csv(
     "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
 )
-# specifing the response variable
+# specifying the response variable
 response_name = " <=50K"
 # Don't include response in other variables, we don't want to plot response vs response.
 col_name = [col for col in dataset.columns if col != response_name]
@@ -52,9 +53,50 @@ def predictor_response_plots(col_name):
         plt.show()
 
 
+def regression_model(col_name):
+    # when the response is categorical (logistic regression)
+    if (dataset[response_name].dtypes == "object") & (
+        dataset[col_name].dtypes == "object"
+    ):
+        x = pd.get_dummies(dataset[col_name])
+        y = pd.get_dummies(dataset[response_name])
+        log_reg = sm.Logit(x, y).fit()
+        print(log_reg.summary)
+
+    if (dataset[response_name].dtypes == "object") & (
+        dataset[col_name].dtypes == "int64"
+    ):
+        x = (dataset[col_name] - dataset[col_name].min()) / (
+            dataset[col_name].max() - dataset[col_name].min()
+        )
+        y = pd.get_dummies(dataset[response_name])
+        log_reg = sm.Logit(x.astype(float), y).fit()
+        print(log_reg.summary)
+
+    # when the response is continuous (regression)
+    if (dataset[response_name].dtypes == "int64") & (
+        dataset[col_name].dtypes == "int64"
+    ):
+        x = dataset[col_name]
+        y = dataset[response_name]
+        x = sm.add_constant(x)
+        model = sm.OLS(y, x).fit()
+        print(model.summary)
+    if (dataset[response_name].dtypes == "int64") & (
+        dataset[col_name].dtypes == "object"
+    ):
+        x = pd.get_dummies(dataset[col_name])
+        y = dataset[response_name]
+        x = sm.add_constant(x)
+        model = sm.OLS(y, x).fit()
+        print(model.summary)
+
+
 def main():
+    # for i in range(1, len(col_name)):
+    #    predictor_response_plots(col_name[i])
     for i in range(1, len(col_name)):
-        predictor_response_plots(col_name[i])
+        regression_model(col_name[i])
 
 
 if __name__ == "__main__":

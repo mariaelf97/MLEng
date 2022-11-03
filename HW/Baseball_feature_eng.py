718848acf28c5048692535595126246fee617214
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-11-02 19:15:52 -0700

    connected to db

---
+++ HW/Baseball_feature_eng.py
@@ -0,0 +1,27 @@
+import sys
+
+import pandas
+import sqlalchemy
+
+
+def main():
+    db_user = ""
+    db_pass = ""  # pragma: allowlist secret
+    db_host = "localhost"
+    db_database = "baseball"
+    connect_string = (
+        f"mariadb+mariadbconnector://{db_user}:{db_pass}@{db_host}/{db_database}"
+    )
+    # pragma: allowlist secret
+
+    sql_engine = sqlalchemy.create_engine(connect_string)
+
+    query = """
+        SELECT * from game;
+    """
+    df = pandas.read_sql_query(query, sql_engine)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    sys.exit(main())

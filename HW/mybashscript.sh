28f86261c8ee5f532b09964ad0d0a8d5fbd497e9
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-02 18:22:42 -0800

    bash script to run commands in the container

---
+++ HW/mybashscript.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sleep 30
+
+mysql -u root -pPassword123 -h mariadb -e "CREATE DATABASE IF NOT EXISTS baseball"
+echo "baseball db created"
+mysql -u root -pPassword123 -h mariadb baseball < /App/baseball.sql
+echo "loaded baseball db data"
+mysql -u root -pPassword123 -h mariadb baseball<test.sql > sql_output.txt
+echo "file created"
+head sql_output.txt
+
+
+

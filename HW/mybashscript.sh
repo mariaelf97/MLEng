ba28c11e6fd69c973b9248c61ecaabb309e0ca3e
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-02 19:07:43 -0800

    changed sql file name

--- HW/mybashscript.sh
+++ HW/mybashscript.sh
@@ -6,7 +6,7 @@ mysql -u root -pPassword123 -h mariadb -e "CREATE DATABASE IF NOT EXISTS basebal
 echo "baseball db created"
 mysql -u root -pPassword123 -h mariadb baseball < /App/baseball.sql
 echo "loaded baseball db data"
-mysql -u root -pPassword123 -h mariadb baseball<test.sql > sql_output.txt
+mysql -u root -pPassword123 -h mariadb baseball<mybaseball.sql > sql_output.txt
 echo "file created"
 head sql_output.txt
 

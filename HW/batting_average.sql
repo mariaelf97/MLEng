c2465e0eff25692a03d4da9acae19e0a4282d778
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-07 20:34:12 -0800

    Rolling sum pitcher stat

--- HW/batting_average.sql
+++ HW/batting_average.sql
@@ -18,8 +18,22 @@ create or replace table joined_game_team_pitching_counts
  select * from joined_game_team_pitching_counts limit 1,10;
 
  select t1.local_date as t1_date
+        , t1.game_id
         , t1.team_id
         , sum(t2.Single)
+        , sum(t2.Double)
+        , sum(t2.Triple)
+        , sum(t2.atBat)
+        , sum(t2.Home_Run)
+        , sum(t2.Hit)
+        , sum(t2.base_on_balls)
+        , sum(t2.plate_apperance)
+        , sum(t2.ground_out)/sum(t2.fly_out) as Go_to_AO
+        , sum(t2.ground_out)
+        , sum(t2.fly_out)
+        , sum(tbc.Strikeout)/sum(t2.Home_Run) as Bo_to_BB
+        , sum(t2.atBat)/sum(t2.Home_Run) as AB_to_HR
+        , sum(t2.atBat)/sum(t2.Hit)
         , COUNT(*) AS cnt
     from joined_game_team_pitching_counts t1
     join joined_game_team_pitching_counts t2 ON

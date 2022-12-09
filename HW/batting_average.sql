c996076e64136005bdbb54acab88099e3cfc05fa
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-08 16:30:19 -0800

    added more features from team batting table

--- HW/batting_average.sql
+++ HW/batting_average.sql
@@ -43,3 +43,31 @@ select t1.local_date as t1_date
 SELECT * FROM rolling_game_team_pitching limit 1,100;
 
 
+create or replace table rolling_game_team_batting
+select t1.local_date as t1_date
+        , t1.game_id
+        , t1.team_id
+        , sum(t2.Single) as batting_single
+        , sum(t2.Double) as batting_double
+        , sum(t2.Triple) as batting_triple
+        , sum(t2.atBat) as batting_atbat
+        , sum(t2.Home_Run) as batting_homerun
+        , sum(t2.Hit) as batting_hit
+        , SUM(t2.Hit_by_Pitch) as batting_hit_by_pitch
+        , sum(t2.Walk) as batting_baseonball_or_walk
+        , IFNULL(sum(t2.atBat)/sum(NULLIF(t2.Home_Run,0)),0) as batting_ab_to_hr
+        , sum(t2.ground_out) as pitching_groundout
+        , sum(t2.Flyout) as pitching_flyout_or_airout
+        , IFNULL(sum(t2.Walk)/sum(NULLIF(t2.Strikeout,0)),0) as batting_w_to_sr
+        , IFNULL(sum(t2.Ground_Out)/sum(NULLIF(t2.Flyout,0)),0) as batting_go_to_fo_or_ao
+        , IFNULL(sum(t2.atBat)/sum(NULLIF(t2.Hit,0)),0) as batting_average_batting
+        , IFNULL(sum(t2.Home_Run)/sum(NULLIF(t2.Hit,0)),0) as batting_hr_to_hit
+        , sum(t2.Hit) + sum(t2.Walk) + sum(t2.Hit_by_Pitch) as times_on_base_or_tob
+        , sum(t2.Single) + sum(t2.Double) + sum(t2.Triple) + sum(t2.Home_Run) as extra_base_hits_or_ebh
+        , COUNT(*) AS cnt
+    from joined_game_team_batting_counts t1
+    join joined_game_team_batting_counts t2 ON
+        t1.local_date > DATE_ADD(t2.local_date , INTERVAL -100 DAY) AND t1.team_id =t2.team_id
+     group by t1.local_date, t1.team_id
+     order by t1.team_id, t1.local_date ASC , t2.local_date ASC;
+SELECT * FROM rolling_game_team_batting limit 1,100;

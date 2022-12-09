ee4f2c8297fc2ea8d175a38746680cd5b37b6c32
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-08 17:43:15 -0800

    joined rolling batting table with pitching table

--- HW/batting_average.sql
+++ HW/batting_average.sql
@@ -1,7 +1,9 @@
+use test;
+
 create or replace table joined_game_team_pitching_counts
  select  		g.local_date
                 ,g.game_id
-                ,tbc.team_id
+                ,tpc.team_id
                 ,tbc.Single
                 ,tbc.Double
                 ,tbc.Triple
@@ -12,7 +14,7 @@ create or replace table joined_game_team_pitching_counts
                 ,tbc.Ground_Out
                 ,tbc.Flyout
                 ,tbc.Strikeout
- from team_pitching_counts tbc
+ from team_pitching_counts tpc
  join game g on tbc.game_id=g.game_id;
 
 select * from joined_game_team_pitching_counts limit 1,100;
@@ -42,6 +44,24 @@ select t1.local_date as t1_date
      order by t1.team_id, t1.local_date ASC , t2.local_date ASC;
 SELECT * FROM rolling_game_team_pitching limit 1,100;
 
+create or replace table joined_game_team_batting_counts
+ select  		  g.local_date
+                , g.game_id
+                , tbc.team_id
+                , tbc.Single
+                , tbc.Double
+                , tbc.Triple
+                , tbc.atBat
+                , tbc.Home_Run
+                , tbc.Hit
+                , tbc.Walk
+                , tbc.Hit_by_Pitch
+                , tbc.Ground_Out
+                , tbc.Flyout
+                , tbc.Strikeout
+    from team_batting_counts tbc
+    join game g on tbc.game_id=g.game_id;
+ select * from joined_game_team_pitching_counts limit 1,10;
 
 create or replace table rolling_game_team_batting
 select t1.local_date as t1_date
@@ -56,8 +76,8 @@ select t1.local_date as t1_date
         , SUM(t2.Hit_by_Pitch) as batting_hit_by_pitch
         , sum(t2.Walk) as batting_baseonball_or_walk
         , IFNULL(sum(t2.atBat)/sum(NULLIF(t2.Home_Run,0)),0) as batting_ab_to_hr
-        , sum(t2.ground_out) as pitching_groundout
-        , sum(t2.Flyout) as pitching_flyout_or_airout
+        , sum(t2.ground_out) as batting_groundout
+        , sum(t2.Flyout) as batting_flyout_or_airout
         , IFNULL(sum(t2.Walk)/sum(NULLIF(t2.Strikeout,0)),0) as batting_w_to_sr
         , IFNULL(sum(t2.Ground_Out)/sum(NULLIF(t2.Flyout,0)),0) as batting_go_to_fo_or_ao
         , IFNULL(sum(t2.atBat)/sum(NULLIF(t2.Hit,0)),0) as batting_average_batting
@@ -71,3 +91,28 @@ select t1.local_date as t1_date
      group by t1.local_date, t1.team_id
      order by t1.team_id, t1.local_date ASC , t2.local_date ASC;
 SELECT * FROM rolling_game_team_batting limit 1,100;
+
+ALTER table rolling_game_team_batting drop cnt;
+
+CREATE or REPLACE TABLE joined_team_batting_pitching
+ select rgtp.*
+ 		,rgtb.batting_single
+ 		,rgtb.batting_double
+ 		,rgtb.batting_triple
+ 		,rgtb.batting_atbat
+ 		,rgtb.batting_homerun
+ 		,rgtb.batting_hit
+ 		,rgtb.batting_hit_by_pitch
+ 		,rgtb.batting_baseonball_or_walk
+ 		,rgtb.batting_ab_to_hr
+ 		,rgtb.batting_groundout
+ 		,rgtb.batting_flyout_or_airout
+ 		,rgtb.batting_w_to_sr
+ 		,rgtb.batting_go_to_fo_or_ao
+ 		,rgtb.batting_average_batting
+ 		,rgtb.batting_hr_to_hit
+ 		,rgtb.times_on_base_or_tob
+ 		,rgtb.extra_base_hits_or_ebh
+ 		from rolling_game_team_pitching rgtp
+ join rolling_game_team_batting rgtb on rgtb.team_id=rgtp.team_id;
+

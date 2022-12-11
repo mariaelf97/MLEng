b6244d8aa07cb022a57ea97f91375588f2458eb8
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-11 11:57:22 -0800

    created index on tables, fixed home or away value

--- HW/batting_average.sql
+++ HW/batting_average.sql
@@ -1,28 +1,44 @@
 use test;
 
+-- create index on game and team_pitching_count table to make join faster
+create or replace INDEX game_local_date_index
+ON game (game_id,local_date)
+;
+
+create or replace INDEX game_id_pitching_index
+ON team_pitching_counts (game_id)
+;
+-- joining game and team_pithcing_counts table to get local date
+-- creating a column that defines if the team was the away team or home team
 create or replace table joined_game_team_pitching_counts
  select  		g.local_date
                 ,g.game_id
                 ,tpc.team_id
-                ,tbc.Single
-                ,tbc.Double
-                ,tbc.Triple
-                ,tbc.atBat
-                ,tbc.Home_Run
-                ,tbc.Hit
-                ,tbc.Walk
-                ,tbc.Ground_Out
-                ,tbc.Flyout
-                ,tbc.Strikeout
+                ,tpc.Single
+                ,tpc.Double
+                ,tpc.Triple
+                ,tpc.atBat
+                ,tpc.Home_Run
+                ,tpc.Hit
+                ,tpc.Walk
+                ,tpc.Ground_Out
+                ,tpc.Flyout
+                ,tpc.Strikeout
+                ,CASE when tpc.team_id = g.away_team_id then 'A'
+                	when tpc.team_id = g.home_team_id then 'H'
+                	else null end as home_or_away
  from team_pitching_counts tpc
- join game g on tbc.game_id=g.game_id;
+ join game g on tpc.game_id=g.game_id;
 
-select * from joined_game_team_pitching_counts limit 1,100;
-
+-- creating index to make self join faster
+create or replace INDEX game_id_local_date_pitching_index
+ON joined_game_team_pitching_counts (game_id,local_date)
+;
 create or replace table rolling_game_team_pitching
 select t1.local_date as t1_date
         , t1.game_id
         , t1.team_id
+        , t1.home_or_away
         , sum(t2.Single) as pitching_single
         , sum(t2.Double) as pitching_double
         , sum(t2.Triple) as pitching_triple
@@ -40,9 +56,8 @@ select t1.local_date as t1_date
     from joined_game_team_pitching_counts t1
     join joined_game_team_pitching_counts t2 ON
         t1.local_date > DATE_ADD(t2.local_date , INTERVAL -100 DAY) AND t1.team_id =t2.team_id
-     group by t1.local_date, t1.team_id
+     group by t1.local_date, t1.team_id, t1.home_or_away
      order by t1.team_id, t1.local_date ASC , t2.local_date ASC;
-SELECT * FROM rolling_game_team_pitching limit 1,100;
 
 create or replace table joined_game_team_batting_counts
  select  		  g.local_date
@@ -94,6 +109,10 @@ SELECT * FROM rolling_game_team_batting limit 1,100;
 
 ALTER table rolling_game_team_batting drop cnt;
 
+create or replace INDEX team_batting_indx
+ON rolling_game_team_batting (game_id,team_id)
+;
+
 CREATE or REPLACE TABLE joined_team_batting_pitching
  select rgtp.*
  		,rgtb.batting_single
@@ -114,5 +133,22 @@ CREATE or REPLACE TABLE joined_team_batting_pitching
  		,rgtb.times_on_base_or_tob
  		,rgtb.extra_base_hits_or_ebh
  		from rolling_game_team_pitching rgtp
- join rolling_game_team_batting rgtb on rgtb.team_id=rgtp.team_id;
+ join rolling_game_team_batting rgtb on rgtb.team_id=rgtp.team_id and rgtb.game_id=rgtp.game_id;
+
+SELECT * from joined_team_batting_pitching limit 1,100;
+
+create or replace INDEX index2
+ON joined_team_batting_pitching (game_id,team_id)
+;
+
+CREATE or REPLACE TABLE joined_team_batting_pitching_boxscore
+ select bs.winner_home_or_away
+ 		,jtbp.*
+ 		from boxscore bs
+ join joined_team_batting_pitching jtbp_home
+ on bs.game_id=jtbp_home.game_id AND jtbp_home.home_or_away='H';
+join joined_team_batting_pitching jtbp_away
+on bs.game_id=jtbp_away.game_id AND jtbp_away.home_or_away='A';
+
+SELECT * FROM joined_team_batting_pitching_boxscore limit 1,100;
 

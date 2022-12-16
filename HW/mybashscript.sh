4f826e74badb81591de67bd03664186390c9e7e1
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-15 19:33:20 -0800

    updated

--- HW/mybashscript.sh
+++ HW/mybashscript.sh
@@ -3,12 +3,12 @@
 sleep 30
 
 mysql -u root -pPassword123 -h mariadb -e "CREATE DATABASE IF NOT EXISTS baseball"
-echo "baseball db created"
+echo "baseball db created, now loading the baseball db...it's very slow"
 mysql -u root -pPassword123 -h mariadb baseball < /App/baseball.sql
-echo "loaded baseball db data"
-mysql -u root -pPassword123 -h mariadb baseball<mybaseball.sql > sql_output.txt
-echo "file created"
-head sql_output.txt
+echo "loaded baseball db data, now running sql...please be patient"
+mysql -u root -pPassword123 -h mariadb baseball<batting_average.sql
+echo "SQL table made successfully, now running the python code.... it's going to be very slow.."
+python3 basbeall_fresh.py
 
 
 

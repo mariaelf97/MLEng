7b287dbab42280f535626b990c9580d7e00c97fd
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2022-12-17 21:05:32 -0800

    fixed typo in the script

--- HW/mybashscript.sh
+++ HW/mybashscript.sh
@@ -2,7 +2,7 @@
 
 sleep 30
 
-mysql -u root -pPassword123 -h mariadb -e "CREATE DATABASE IF NOT EXISTS baseball"
+  mysql -u root -pPassword123 -h mariadb -e "CREATE DATABASE IF NOT EXISTS baseball"
 echo "baseball db created, now loading the baseball db...it's very slow..slower than molasses"
 mysql -u root -pPassword123 -h mariadb baseball < /app/baseball.sql
 echo "loaded baseball db data, now running sql...please be patient..maybe get some tea.."

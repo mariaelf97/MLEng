d4a602c0504a3f87abcb71e2adedb8f0f3881747
Maryam Ahmadi J <39787943+mariaelf97@users.noreply.github.com>
2022-12-17 19:13:31 -0800

    changed directory name App to app

--- HW/mybashscript.sh
+++ HW/mybashscript.sh
@@ -4,7 +4,7 @@ sleep 30
 
 mysql -u root -pPassword123 -h mariadb -e "CREATE DATABASE IF NOT EXISTS baseball"
 echo "baseball db created, now loading the baseball db...it's very slow..slower than molasses"
-mysql -u root -pPassword123 -h mariadb baseball < /App/baseball.sql
+mysql -u root -pPassword123 -h mariadb baseball < /app/baseball.sql
 echo "loaded baseball db data, now running sql...please be patient..maybe get some tea.."
 mysql -u root -pPassword123 -h mariadb baseball<batting_average.sql
 echo "SQL table made successfully, now running the python code.... it's going to be very slow.."
